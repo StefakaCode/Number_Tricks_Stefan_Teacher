613e6c45d17d2e71eb4aa98169423c4edfe69673
Tonziss <antonatanasov2002@gmail.com>
2025-03-10 12:01:24 +0200

    add FastExponentiate method to NumberUtils

--- NumberUtils.cs
+++ NumberUtils.cs
@@ -2,7 +2,17 @@
 
 public class NumberUtils
 {
-	public NumberUtils()
-	{
-	}
+    public NumberUtils()
+    {
+
+    }
+
+    public static double FastExponentiate(double a, int power)
+    {
+        if (power == 0) return 1;
+        double halfPowered = FastExponentiate(a, power / 2);
+        double subTotal = halfPowered * halfPowered;
+        return power % 2 == 0 ? subTotal : subTotal * a;
+
+    }
 }
