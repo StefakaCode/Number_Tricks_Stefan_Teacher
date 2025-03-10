731bead0a287415151355660a2ea599e0c74e584
MEGA <MEGA@gmail.com>
2025-03-10 12:14:55 +0200

    Added Babylon method

--- NumberUtils.cs
+++ NumberUtils.cs
@@ -4,5 +4,19 @@ public class NumberUtils
 {
 	public NumberUtils()
 	{
+
 	}
+	public double Babylon(float n)
+	{
+        float x = n;
+        float y = 1;
+
+        double e = 0.000001;
+        while (x - y > e)
+        {
+            x = (x + y) / 2;
+            y = n / x;
+        }
+        return x;
+    }
 }
