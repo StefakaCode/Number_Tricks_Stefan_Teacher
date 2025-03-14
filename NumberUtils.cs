2a0fe150dee39f061d6862a844d33155e87a51a6
Tonziss <antonatanasov2002@gmail.com>
2025-03-14 18:13:30 +0200

    implement eratosthenes sieve

--- NumberUtils.cs
+++ NumberUtils.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.IO.IsolatedStorage;
 
 public class NumberUtils
 {
-	public static double Babylon(float n)
-	{
+    public static double Babylon(float n)
+    {
         float x = n;
         float y = 1;
         double e = 0.000001;
@@ -14,7 +15,7 @@ public class NumberUtils
         }
         return x;
     }
-  
+
     public static List<int> FindDivisors(int n)
     {
         List<int> divisors = new List<int>();
@@ -33,7 +34,7 @@ public class NumberUtils
         divisors.Sort();
         return divisors;
     }
-        
+
     public static double FastExponentiate(double a, int power)
     {
         if (power == 0) return 1;
@@ -41,5 +42,22 @@ public class NumberUtils
         double subTotal = halfPowered * halfPowered;
         return power % 2 == 0 ? subTotal : subTotal * a;
 
+    }
+
+    public static int[] EratoshtenesSieve(int n)
+    {
+        bool[] isPrime = new bool[n]; // bool for each number from 1 to n
+        for (int i = 1; i < n; i++) // i=0 would be for the number one, but one is not prime
+            isPrime[i] = true;
+
+
+        //from 2 to n
+        for (int i = 1; i < n; i++)
+            for (int j = i + 1; j < n; j++)
+                if (isPrime[j] && (j + 1) % (i + 1) == 0)
+                    isPrime[j] = false;
+
+        int[] primes = isPrime.Select((isPrime,i) => isPrime ? i+1: 0 ).Where(n=>n!=0).ToArray();
+        return primes;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
-Console.Write(NumberUtils.Babylon(2));
-
+Console.WriteLine(NumberUtils.Babylon(1024));
+Console.WriteLine(NumberUtils.FastExponentiate(2,10));
+Console.WriteLine(String.Join(" ", NumberUtils.EratoshtenesSieve(100)));
