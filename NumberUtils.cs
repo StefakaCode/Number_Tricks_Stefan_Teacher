0df4ad4a4f762cc9379be3f1d05d42ff1931c5dd
MEGA <MEGA@gmail.com>
2025-03-14 12:23:52 +0200

    Added FindDivisors

--- NumberUtils.cs
+++ NumberUtils.cs
@@ -6,7 +6,25 @@ public class NumberUtils
     {
 
     }
+    public static List<int> FindDivisors(int n)
+    {
+        List<int> divisors = new List<int>();
 
+        for (int i = 1; i * i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                divisors.Add(i);
+                if (i != n / i)
+                {
+                    divisors.Add(n / i);
+                }
+            }
+        }
+
+        divisors.Sort();
+        return divisors;
+    }
     public static double FastExponentiate(double a, int power)
     {
         if (power == 0) return 1;
