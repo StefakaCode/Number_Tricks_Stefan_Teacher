de5dc776748666e9de92ee3adf21b2337021f48c
mega <mega@MEGA-RM05>
2025-03-17 11:38:29 +0200

    Added PascalTriangle method

--- NumberUtils.cs
+++ NumberUtils.cs
@@ -6,7 +6,19 @@ public class NumberUtils
     {
 
     }
-
+    public static void PrintPascalTriangle(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            int num = 1;
+            for (int j = 0; j <= i; j++)
+            {
+                Console.Write(num + " ");
+                num = num * (i - j) / (j + 1);
+            }
+            Console.WriteLine();
+        }
+    }
     public static double FastExponentiate(double a, int power)
     {
         if (power == 0) return 1;
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
-
+Console.Write("n: ");
+int n = int.Parse(Console.ReadLine());
+NumberUtils.PrintPascalTriangle(n); 
